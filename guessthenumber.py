e903f4abcd189bcf2ba731cd76986d5152e98a90
Kyle Marion <73936202+KyleMarion0@users.noreply.github.com>
2022-10-27 22:48:47 -0400

    Final version. May make some improvements.

--- guessthenumber.py
+++ guessthenumber.py
@@ -6,31 +6,43 @@ user_attempts_hard = 5
 #Create a function for difficulty.
 def set_difficulty():
   easy_or_hard = input('Choose a difficulty. Type "easy" or "hard": ')
+  easy_or_hard.lower()
   if easy_or_hard == 'easy':
     return user_attempts_easy
   elif easy_or_hard == 'hard':
     return user_attempts_hard
 
 #Function to check the users guess.
-def check_user_guess(user_guess, random_num):
+def check_user_guess(user_guess, random_num, attempts):
+  ''' Checks random number against user guess. Returns remaining attempts.'''
   if user_guess > random_num:
     print('Too high.')
+    return attempts - 1
   elif user_guess < random_num:
     print('Too low.')
+    return attempts - 1
   else:
     print(f'You got it! The answer was {random_num}.')
 
 #Choosing a random number between 1 and 100.
 random_num = random.randint(1, 100)
 
-#Welcome messages.
-print("Welcome to the number guessing game!")
-print("I'm thinking of a number between 1 and 100.")
-print(f"Pssst, cheater! The correct answer is {random_num}.")
+def game():
+  #Welcome messages.
+  print("Welcome to the number guessing game!")
+  print("I'm thinking of a number between 1 and 100.")
+  print(f"Pssst, cheater! The correct answer is {random_num}.")
 
-#Choose difficulty.
-tries_remaining = set_difficulty()
-print(f'You have {tries_remaining} attempts remaining to guess the number.')
-
-#User guess.
-user_guess = int(input("Make a guess: "))
+  #Keep track of guesses remaining.
+  tries_remaining = set_difficulty()
+  user_guess = 0
+  attempts = 0
+  while user_guess != random_num:
+    print(f'You have {tries_remaining} attempts remaining to guess the number.')
+    user_guess = int(input("Make a guess: "))
+    tries_remaining = check_user_guess(user_guess, random_num, tries_remaining)
+    if tries_remaining == 0:
+      print("You've run out of guesses, you lose.")
+      return
+    
+game()
